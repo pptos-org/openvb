468a5503763cd591889720cff8ef16dfe0b8793c
quintenvandamme <quintenvandamme@proton.me>
2024-08-15 14:20:06 +0200

    Add windows support

---
+++ build.bat
@@ -0,0 +1,27 @@
+@echo off
+
+:: Get the architecture
+set ARCH=%PROCESSOR_ARCHITECTURE%
+
+:: Replace "AMD64" with "x64"
+if "%ARCH%"=="AMD64" (
+    set ARCH=x64
+)
+
+:: Replace "x86" with "x86"
+if "%ARCH%"=="x86" (
+    set ARCH=x86
+)
+
+:: Replace "ARM64" with "aarch64"
+if "%ARCH%"=="ARM64" (
+    set ARCH=aarch64
+)
+
+:: Replace "ARM" with "arm"
+if "%ARCH%"=="ARM" (
+    set ARCH=arm
+)
+
+dart pub get
+dart compile exe bin/openvb.dart -o bin/openvb-windows-%ARCH%.exe
